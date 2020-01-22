5ebf2859563703191e1da62a81718a7fac251182
Mark Bates <mark@markbates.com>
2020-01-22 13:12:26 -0500

    Update bzr.go

--- cli/internal/plugins/bzr/bzr.go
+++ cli/internal/plugins/bzr/bzr.go
@@ -1,9 +1,9 @@
 package bzr
 
 import (
-	"github.com/gobuffalo/buffalo-cli/cli/internal/plugins/buildcmd"
-	"github.com/gobuffalo/buffalo-cli/plugins"
-	"github.com/gobuffalo/buffalo-cli/plugins/plugprint"
+	"github.com/gobuffalo/buffalo-cli/v2/cli/internal/plugins/buildcmd"
+	"github.com/gobuffalo/buffalo-cli/v2/plugins"
+	"github.com/gobuffalo/buffalo-cli/v2/plugins/plugprint"
 )
 
 var _ buildcmd.Versioner = &Versioner{}
