f47e576f21a0f5e5247174ca804f69d1223c963a
Mark Bates <mark@markbates.com>
2020-04-05 11:47:05 -0400

    clean up fizz

--- cli/internal/plugins/fizz/fizz.go
+++ cli/internal/plugins/fizz/fizz.go
@@ -1,13 +1,14 @@
 package fizz
 
 import (
+	"github.com/gobuffalo/buffalo-cli/v2/cli/internal/plugins/fizz/generate"
 	"github.com/gobuffalo/buffalo-cli/v2/cli/internal/plugins/fizz/setup"
 	"github.com/gobuffalo/plugins"
 )
 
 func Plugins() []plugins.Plugin {
 	return []plugins.Plugin{
-		&MigrationGen{},
+		&generate.Migration{},
 		&setup.Setup{},
 	}
 }
