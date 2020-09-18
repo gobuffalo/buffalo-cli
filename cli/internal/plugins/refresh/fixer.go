55df1fa6feda82dbaf3b74a2e623bb381da56121
Antonio Pagano <apagano@wawand.co>
2020-09-18 10:30:30 -0500

    adding some comments

--- cli/internal/plugins/refresh/fixer.go
+++ cli/internal/plugins/refresh/fixer.go
@@ -10,12 +10,16 @@ import (
 	"github.com/markbates/refresh/refresh"
 )
 
+// Fixer will fix current .buffalo.dev.yml to build cmd/[name]/ instead of
+// expecting the main to be in the root folder.
 type Fixer struct{}
 
 func (*Fixer) PluginName() string {
 	return "refresh/fixer"
 }
 
+// Fix changes .buffalo.dev.yml to build cmd/[name] if it exists, otherwise
+// it creates the file in the root of the project.
 func (f *Fixer) Fix(ctx context.Context, root string, args []string) error {
 
 	info, err := here.Dir(root)
