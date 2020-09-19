a7350c8e679901dfcf61cb1c48a87f286ee2362e
Antonio Pagano <apagano@wawand.co>
2020-09-19 16:35:36 -0500

    back to cmd/[name]

--- cli/internal/plugins/golang/mainfix/cmd.go
+++ cli/internal/plugins/golang/mainfix/cmd.go
@@ -3,7 +3,10 @@ package mainfix
 import (
 	"context"
 	"os"
+	"path"
 	"path/filepath"
+
+	"github.com/gobuffalo/here"
 )
 
 // Cmd takes care of moving existing main.go into
@@ -15,7 +18,12 @@ func (*Cmd) PluginName() string {
 }
 
 func (fixer *Cmd) Fix(ctx context.Context, root string, args []string) error {
-	fp := filepath.Join(root, "cmd", "main")
+	info, err := here.Dir(root)
+	if err != nil {
+		return err
+	}
+
+	fp := filepath.Join(root, "cmd", path.Base(info.Module.Path))
 	if _, err := os.Stat(filepath.Join(fp, "main.go")); err == nil {
 		return nil
 	}
